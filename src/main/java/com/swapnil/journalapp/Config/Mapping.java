2605b8bf0ab416a140fb4417d543347614f467b1
Swapnil Devkate <swapnildevkategmi@gmail.com>
2026-03-29 16:58:30 +0530

    Open This SRC folder

--- src/main/java/com/swapnil/journalapp/Config/Mapping.java
+++ src/main/java/com/swapnil/journalapp/Config/Mapping.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Mapping {
-
+    //This is 3rd party dependency to convert dto to entity and entity to dto
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
